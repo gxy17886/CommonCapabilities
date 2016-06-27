a8f1a55d7d73f5faa1c99072b0e525410f77990c
Ruben Taelman <rubensworks@gmail.com>
2016-06-27 20:35:55 +0200

    Make IC2 EU and Tesla integration more generic Placing a Tesla generator next an Electric Furnace for example will work just fine.

--- src/main/java/org/cyclops/commoncapabilities/modcompat/ic2/capability/tesla/Ic2TeslaIntegration.java
+++ src/main/java/org/cyclops/commoncapabilities/modcompat/ic2/capability/tesla/Ic2TeslaIntegration.java
@@ -2,14 +2,13 @@ package org.cyclops.commoncapabilities.modcompat.ic2.capability.tesla;
 
 import ic2.api.item.IElectricItem;
 import ic2.api.tile.IEnergyStorage;
+import ic2.core.block.TileEntityBlock;
 import net.darkhax.tesla.api.ITeslaHolder;
 import net.darkhax.tesla.capability.TeslaCapabilities;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import org.cyclops.commoncapabilities.CommonCapabilities;
-import org.cyclops.commoncapabilities.modcompat.ic2.capability.tesla.HolderProducerConsumerElectricItem;
-import org.cyclops.commoncapabilities.modcompat.ic2.capability.tesla.HolderProducerConsumerEnergyStorage;
 import org.cyclops.cyclopscore.modcompat.capabilities.CapabilityConstructorRegistry;
 import org.cyclops.cyclopscore.modcompat.capabilities.ICapabilityConstructor;
 import org.cyclops.cyclopscore.modcompat.capabilities.MultipleCapabilityProvider;
@@ -38,6 +37,24 @@ public class Ic2TeslaIntegration {
                     @Override
                     public ICapabilityProvider createProvider(IEnergyStorage host) {
                         HolderProducerConsumerEnergyStorage capabilityImplementation = new HolderProducerConsumerEnergyStorage(host);
+                        return MultipleCapabilityProvider.of(
+                                TeslaCapabilities.CAPABILITY_HOLDER, capabilityImplementation,
+                                TeslaCapabilities.CAPABILITY_PRODUCER, capabilityImplementation,
+                                TeslaCapabilities.CAPABILITY_CONSUMER, capabilityImplementation
+                        );
+                    }
+                });
+        registry.registerInheritableTile(TileEntityBlock.class,
+                new SimpleCapabilityConstructor<ITeslaHolder, TileEntityBlock>() {
+                    @Override
+                    public Capability<ITeslaHolder> getCapability() {
+                        return TeslaCapabilities.CAPABILITY_HOLDER;
+                    }
+
+                    @Nullable
+                    @Override
+                    public ICapabilityProvider createProvider(TileEntityBlock host) {
+                        HolderProducerConsumerBlock capabilityImplementation = new HolderProducerConsumerBlock(host);
                         return MultipleCapabilityProvider.of(
                                 TeslaCapabilities.CAPABILITY_HOLDER, capabilityImplementation,
                                 TeslaCapabilities.CAPABILITY_PRODUCER, capabilityImplementation,
